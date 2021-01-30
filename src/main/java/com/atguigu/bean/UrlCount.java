765a46ed080c1da3e98e1022e8e89d31bc9a723f
harry <harry@atguigu.com>
2021-01-30 10:36:54 +0800

    day08(上午 1)

---
+++ src/main/java/com/atguigu/bean/UrlCount.java
@@ -0,0 +1,14 @@
+package com.atguigu.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UrlCount {
+    private String url;
+    private Long windowEnd;
+    private Integer count;
+}
