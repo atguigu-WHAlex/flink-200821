5645c7efd75b606a326d80f97df282aaaf9be0f3
harry <harry@atguigu.com>
2021-01-30 12:09:59 +0800

    day08(上午 2)

---
+++ src/main/java/com/atguigu/bean/AdsClickLog.java
@@ -0,0 +1,16 @@
+package com.atguigu.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdsClickLog {
+    private Long userId;
+    private Long adId;
+    private String province;
+    private String city;
+    private Long timestamp;
+}
