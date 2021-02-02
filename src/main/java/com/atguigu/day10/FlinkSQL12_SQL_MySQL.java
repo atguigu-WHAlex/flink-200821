6c0b86ba1504adbca3637285e8323a9f11d618e5
harry <harry@atguigu.com>
2021-02-02 17:04:02 +0800

    day10(下午 2)

---
+++ src/main/java/com/atguigu/day10/FlinkSQL12_SQL_MySQL.java
@@ -0,0 +1,43 @@
+package com.atguigu.day10;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+public class FlinkSQL12_SQL_MySQL {
+
+    public static void main(String[] args) throws Exception {
+
+        //1.获取执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+        //2.注册SourceTable
+        tableEnv.executeSql("create table source_sensor (id string, ts bigint, vc int) with("
+                + "'connector' = 'kafka',"
+                + "'topic' = 'topic_source',"
+                + "'properties.bootstrap.servers' = 'hadoop102:9092,hadoop103:9092,hadoop104:9092',"
+                + "'properties.group.id' = 'atguigu',"
+                + "'scan.startup.mode' = 'latest-offset',"
+                + "'format' = 'csv'"
+                + ")");
+
+        //3.注册SinkTable：MySQL,表并不会自动创建
+        tableEnv.executeSql("create table sink_sensor (id string, ts bigint, vc int) with("
+                + "'connector' = 'jdbc',"
+                + "'url' = 'jdbc:mysql://hadoop102:3306/test',"
+                + "'table-name' = 'sink_table',"
+                + "'username' = 'root',"
+                + "'password' = '000000'"
+                + ")");
+
+//        //4.执行查询Kafka数据
+//        Table source_sensor = tableEnv.from("source_sensor");
+//        //5.将数据写入MySQL
+//        source_sensor.executeInsert("sink_sensor");
+        tableEnv.executeSql("insert into sink_sensor select * from source_sensor");
+
+    }
+
+}
