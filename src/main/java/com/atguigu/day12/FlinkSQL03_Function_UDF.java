e53045589dd1b821cc9aaf3007678f67b3b49d2c
harry <harry@atguigu.com>
2021-02-04 11:06:26 +0800

    day12(上午 1)

---
+++ src/main/java/com/atguigu/day12/FlinkSQL03_Function_UDF.java
@@ -0,0 +1,59 @@
+package com.atguigu.day12;
+
+import com.atguigu.bean.WaterSensor;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.functions.ScalarFunction;
+
+import static org.apache.flink.table.api.Expressions.$;
+import static org.apache.flink.table.api.Expressions.call;
+
+public class FlinkSQL03_Function_UDF {
+
+    public static void main(String[] args) throws Exception {
+
+        //1.获取执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
+
+        //2.读取端口数据并转换为JavaBean
+        SingleOutputStreamOperator<WaterSensor> waterSensorDS = env.socketTextStream("hadoop102", 9999)
+                .map(line -> {
+                    String[] split = line.split(",");
+                    return new WaterSensor(split[0],
+                            Long.parseLong(split[1]),
+                            Integer.parseInt(split[2]));
+                });
+
+        //3.将流转换为动态表
+        Table table = tableEnv.fromDataStream(waterSensorDS);
+
+        //4.不注册函数直接使用
+//        table.select(call(MyLength.class, $("id"))).execute().print();
+
+        //5.先注册再使用
+        tableEnv.createTemporarySystemFunction("mylen", MyLength.class);
+
+        //TableAPI
+//        table.select($("id"), call("mylen", $("id"))).execute().print();
+
+        //SQL
+        tableEnv.sqlQuery("select id,mylen(id) from " + table).execute().print();
+
+        //6.执行任务
+        env.execute();
+
+    }
+
+    public static class MyLength extends ScalarFunction {
+
+        public int eval(String value) {
+            return value.length();
+        }
+
+    }
+
+}
