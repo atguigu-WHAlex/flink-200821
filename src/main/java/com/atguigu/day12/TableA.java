14dc998af2bb6e4afd8cd3522fcd4f2bd351e4a3
harry <harry@atguigu.com>
2021-02-04 16:42:40 +0800

    day12(下午 2)

---
+++ src/main/java/com/atguigu/day12/TableA.java
@@ -0,0 +1,13 @@
+package com.atguigu.day12;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TableA {
+    private String id;
+    private String name;
+}
---
+++ src/main/java/com/atguigu/day12/TableB.java
@@ -0,0 +1,13 @@
+package com.atguigu.day12;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TableB {
+    private String id;
+    private int classId;
+}
